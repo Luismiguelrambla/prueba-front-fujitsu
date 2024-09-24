03ff6b06342e29e48cc19cdadd39f49e585e09c0
luismiguelrambla <luismiguelrambla@gmail.com>
2024-09-24 17:06:34 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      Prueba front-end developer
+    </div>
+  )
+}
+
+export default App
